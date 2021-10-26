77c5d551f125dfec84a092a26920018625688a6c
Rodrigo Plotze <rplotze@unaerp.br>
2021-10-26 10:47:01 -0300

    Primeiro commit

--- src/main/java/view/FormLogin.java
+++ src/main/java/view/FormLogin.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package view;
 
-/**
- *
- * @author android
- */
+
 public class FormLogin extends javax.swing.JFrame {
 
     /**
